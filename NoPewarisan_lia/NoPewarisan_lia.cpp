4061e05502dda6affea6915270d962cccd244155
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-28 10:42:32 +0700

    Membuat class dari base class

---
+++ NoPewarisan_lia/NoPewarisan_lia.cpp
@@ -0,0 +1,19 @@
+// NoPewarisan_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
