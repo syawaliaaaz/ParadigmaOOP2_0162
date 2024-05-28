0432ba23cba3875dfda7e37d74b29371454598b0
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-28 11:07:31 +0700

    Memasukkan library dan membuat class dari abstraksi yang disertai string

---
+++ abstraksi_lia/abstraksi_lia.cpp
@@ -0,0 +1,28 @@
+// abstraksi_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
