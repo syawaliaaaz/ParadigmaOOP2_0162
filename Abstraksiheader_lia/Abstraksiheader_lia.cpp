8f29e382214c0afb4bc3e1f4295a1b2beb66bd62
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-28 11:17:30 +0700

    Memasukkan library disertai header library math.h serta memasukkan integer main dengan power

---
+++ Abstraksiheader_lia/Abstraksiheader_lia.cpp
@@ -0,0 +1,16 @@
+// Abstraksiheader_lia.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    x = pow(2, 3);
+
+    cout << x << endl;
+}
+
