3b52f2c14262e3cc578318fb802fd598640c670a
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-28 10:28:31 +0700

    Integer Main sampai return 0

--- Paradigmaoop2_lia/Paradigmaoop2_lia.cpp
+++ Paradigmaoop2_lia/Paradigmaoop2_lia.cpp
@@ -37,10 +37,24 @@ public:
     }
 };
 
-
-
 int main()
 {
-    std::cout << "Hello World!\n";
+    seseorang *obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+
+    //akses method pesan() dari class seseorang
+    // a.seseorang::pesan();
+
+    return 0;
 }
+
+
+
+
 
