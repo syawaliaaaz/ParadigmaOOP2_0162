54b3b729df185711a3bc1c8132d496906568bffe
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-28 10:36:27 +0700

    Membuat class dari derived class

--- Overriding_lia/Overriding_lia.cpp
+++ Overriding_lia/Overriding_lia.cpp
@@ -16,6 +16,14 @@ public:
     }
 };
 
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
 
 int main()
 {
