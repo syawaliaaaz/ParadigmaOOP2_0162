6a0360c86613492bb32604356977bd6369a60e8f
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-28 10:46:00 +0700

    Membuat class dari baseclass

--- NoPewarisan_lia/NoPewarisan_lia.cpp
+++ NoPewarisan_lia/NoPewarisan_lia.cpp
@@ -13,6 +13,15 @@ public:
     }
 };
 
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
